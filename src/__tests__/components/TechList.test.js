3b92451493c901b64f4a3590787d6c063b03a577
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-01-21 20:28:10 -0300

    Testando formulario

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -6,11 +6,21 @@ import TechList from '~/components/TechList';
 describe('TechList component', () => {
 
     it('should be able to add new tech', () => {
-        const { getByText, getByTestId, debug } = render(<TechList />);
-        debug();
-        fireEvent.click(getByText('Adicionar'));
-        debug();
+        const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+       
+        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' }});
+        fireEvent.submit(getByTestId('tech-form'));
+        
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+        expect(getByLabelText('Tech')).toHaveValue('');
     });
+
+    // it('should be able to add new tech', () => {
+    //     const { getByText, getByTestId, debug } = render(<TechList />);
+    //     debug();
+    //     fireEvent.click(getByText('Adicionar'));
+    //     debug();
+    //     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    // });
 
 });
